ceb232d5bc362fbd4b2dc3f0391f8447d7480fb7
alien <alien1096028242@gmail.com>
2026-02-17 17:15:40 +0800

    Update plugin-main.cpp

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -1,25 +1,17 @@
-/*
-obs-text-slideshow
-Copyright (C) 2021 Joshua Wong jbwong05@gmail.com
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program. If not, see <https://www.gnu.org/licenses/>
-*/
-
 #include <obs-module.h>
-#include <obs-frontend-api.h>
-#include <QMainWindow>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <obs-frontend-api.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <QMainWindow>
+#include <QDockWidget>
 #include "plugin-macros.generated.h"
 #include "obs-text-slideshow-dock.h"
 
@@ -33,24 +25,30 @@ extern void load_text_gdiplus_slideshow();
 
 bool obs_module_load(void)
 {
-	load_text_freetype2_slideshow();
+    load_text_freetype2_slideshow();
 #ifdef _WIN32
-	load_text_gdiplus_slideshow();
+    load_text_gdiplus_slideshow();
 #endif
 
-	const auto main_window =
-		static_cast<QMainWindow *>(obs_frontend_get_main_window());
-	obs_frontend_push_ui_translation(obs_module_get_string);
-	auto *tmp = new TextSlideshowDock(main_window);
-	obs_frontend_add_dock(tmp);
-	obs_frontend_pop_ui_translation();
+    const auto main_window =
+        static_cast<QMainWindow *>(obs_frontend_get_main_window());
+    
+    if (main_window) {
+        obs_frontend_push_ui_translation(obs_module_get_string);
+        
+        auto *tmp = new TextSlideshowDock(main_window);
+        
+        obs_frontend_add_custom_qdock("TextSlideshow", static_cast<void*>(tmp));
+        
+        obs_frontend_pop_ui_translation();
+    }
 
-	blog(LOG_INFO, "plugin loaded successfully (version %s)",
-	     PLUGIN_VERSION);
-	return true;
+    blog(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+    
+    return true;
 }
 
 void obs_module_unload()
 {
-	blog(LOG_INFO, "plugin unloaded");
+    blog(LOG_INFO, "plugin unloaded");
 }
